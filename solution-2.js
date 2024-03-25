de018bf314e6a0c384ee0630a7191d8f96b60c19
adamomondi <omondiadam19@gmail.com>
2024-03-25 12:03:15 +0300

    added code

--- solution-2.js
+++ solution-2.js
@@ -17,6 +17,7 @@ function checkSpeed(speed) {
             return `points: ${demeritPoints}`
         }
     }
+    5
 }
 const speed=parseInt(prompt("enter the speed; "))
 console.log(checkSpeed(speed))
